6eaa845181cfcb394ac66bfa197b411874d69b0f
Yaren Yuvka <yarenyuvka@gmail.com>
2021-01-31 00:30:44 +0300

    Add project files.

---
+++ Entities/Abstract/IEntity.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Entities.Abstract
+{
+    //IEntity implement eden class bir veri tabanı tablosudur.
+    public interface IEntity
+    {
+
+
+    }
+}
